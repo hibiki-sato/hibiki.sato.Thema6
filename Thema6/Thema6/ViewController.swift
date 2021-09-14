f2f5367b223ebac19e26bec5e0fd7e4d61c069ca
akio0911 <akio0911@gmail.com>
2021-09-15 08:25:15 +0900

    feedback

--- Thema6/Thema6/ViewController.swift
+++ Thema6/Thema6/ViewController.swift
@@ -12,29 +12,33 @@ class ViewController: UIViewController {
     @IBOutlet private weak var numLabel: UILabel!
     @IBOutlet private weak var slider: UISlider!
 
-    var num = String(Int.random(in: 1 ... 100))
+    private var num = Int.random(in: 1...100)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         // 起動時にランダム値を生成
-        numLabel.text = num
+        numLabel.text = String(num)
     }
 
     private func presentAlert(message: String) {
         let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
-        let reChalenge = UIAlertAction(title: "再挑戦", style: .default, handler: { [self]_ in
-            num = String(Int.random(in: 1 ... 100))
-            numLabel.text = num
+        let reChalenge = UIAlertAction(title: "再挑戦", style: .default, handler: { [weak self] _ in
+            self?.resetGame()
         })
         alert.addAction(reChalenge)
         present(alert, animated: true, completion: nil)
     }
 
+    private func resetGame() {
+        num = Int.random(in: 1...100)
+        numLabel.text = String(num)
+    }
+
     @IBAction private func didTapJudgeButton(_ sender: Any) {
-        if num == String(self.slider.value) {
+        if num == Int(slider.value) {
             presentAlert(message: "あたり!\nあなたの値：\(Int(self.slider.value))")
-        } else if num != String(self.slider.value) {
+        } else {
             presentAlert(message: "はずれ！\nあなたの値：\(Int(self.slider.value))")
         }
     }
